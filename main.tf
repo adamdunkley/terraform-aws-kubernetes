4c350fd811aba5d11f94bfa12656c6d31db652e5
Adam Dunkley <adamdunkley@users.noreply.github.com>
2018-05-02 23:37:04 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,4 @@
 #####
-# AWS Prodvider
-#####
-
-# Retrieve AWS credentials from env variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY	
-provider "aws" {
-  profile = "${var.aws_profile}"
-  region = "${var.aws_region}"	
-}
-
-#####
 # Generate kubeadm token
 #####
 
